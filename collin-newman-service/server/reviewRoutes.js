36ebc8e83442192a4c314bdd8045183f5cf46f3a
Chris_Cunningham <this.cunningham@gmail.com>
2021-01-22 21:36:01 -0700

    All services running on proxy

--- collin-newman-service/server/reviewRoutes.js
+++ collin-newman-service/server/reviewRoutes.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const Reviews = require('../database/Reviews.js');
+
+const app = express();
+
+app.use(cors());
 
 const router = express.Router();
 
