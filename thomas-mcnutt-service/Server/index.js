7a9cc237ce9b6cb36d15f581e042235b33a373dc
Chris_Cunningham <this.cunningham@gmail.com>
2021-01-28 16:05:55 -0700

    Thomas image perfected chris image done

--- thomas-mcnutt-service/Server/index.js
+++ thomas-mcnutt-service/Server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(`${__dirname}/../client/dist`));
+app.use(express.static(`${__dirname}/../Client/dist`));
 
 // LOCATION API
 app.get('/api/location', (req, res) => {
