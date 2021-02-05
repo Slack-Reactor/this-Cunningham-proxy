5e29bf42d85575970df6bebbea1f9500b292c6ca
Chris_Cunningham <this.cunningham@gmail.com>
2021-02-05 15:29:01 -0700

    Add collins localhost to routes

--- collin-newman-service/database/index.js
+++ collin-newman-service/database/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const db = (database) => (
-  mongoose.connect(`mongodb://mongo:27017/${database}`, { useNewUrlParser: true }, { useUnifiedTopology: true })
+  // mongoose.connect(`mongodb://mongo:27017/${database}`, { useNewUrlParser: true }, { useUnifiedTopology: true })
+  mongoose.connect(`mongodb://localhost/${database}`, { useNewUrlParser: true }, { useUnifiedTopology: true })
 );
 module.exports = db;
