63d100b44c8fbf9e31642239efd89b2501c9f408
Chris_Cunningham <this.cunningham@gmail.com>
2021-02-05 15:21:14 -0700

    Chris' localhost routes added

--- this-Cunningham-service/db/index.js
+++ this-Cunningham-service/db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-module.exports.connect = mongoose.connect('mongodb://mongo:27017/tripAdvisor', {
+// module.exports.connect = mongoose.connect('mongodb://mongo:27017/tripAdvisor', {
+module.exports.connect = mongoose.connect('mongodb://localhost/tripAdvisor', {
   useNewUrlParser: true, useUnifiedTopology: true,
 })
   .then(() => console.log('Success connecting to mongo Trip Advisor'))
